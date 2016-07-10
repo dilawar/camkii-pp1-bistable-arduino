a52188ef7deb9ce23fab49c404625e11df6b1138
Dilawar Singh <dilawars@ncbs.res.in>
2016-07-10 18:07:58 +0530

    Blinks instead of emitting light.

--- src/noise_detector.ino
+++ src/noise_detector.ino
@@ -14,12 +14,37 @@
  *        License:  GNU GPL2
  */
 
+#define WINDOW_SIZE 20
+
+/**
+ * @brief Keep the running values of signal.
+ */
+int signal_[WINDOW_SIZE];
+
+/*  Running mean of signal. */
+float running_mean_ = 0.0;
+
+double computeMean(  )
+{
+    double sum = 0.0;
+    for (size_t i = 0; i < WINDOW_SIZE; i++) 
+        sum += signal_[i];
+    running_mean_ = sum / WINDOW_SIZE;
+    return running_mean_;
+}
+
+void appendToSignal( int sensorValue )
+{
+    for (size_t i = 0; i < WINDOW_SIZE - 1; i++) 
+        signal_[i] = signal_[i+1];
+    signal_[WINDOW_SIZE-1] = sensorValue;
+}
 
 // the setup routine runs once when you press reset:
 void setup()
 {
     // initialize serial communication at 9600 bits per second:
-    Serial.begin(9600);
+    Serial.begin(19200);
 
     pinMode( A2, OUTPUT );
     pinMode( 13, OUTPUT );
@@ -29,6 +54,10 @@ void setup()
     pinMode( A1, OUTPUT );
     digitalWrite( A1, LOW );
 
+    /* Initialize signal  */
+    for (size_t i = 0; i < WINDOW_SIZE; i++) 
+        signal_[i] = 10;
+
 }
 
 // the loop routine runs over and over again forever:
@@ -36,13 +65,33 @@ void loop()
 {
     // read the input on analog pin 0:
     int sensorValue = analogRead(A0);
-    // print out the value you read:
-    if( sensorValue > 100 )
+    appendToSignal( sensorValue );
+    computeMean( );
+
+#if 0
+    for (size_t i = 0; i < WINDOW_SIZE; i++) 
+    {
+        Serial.print( signal_[i] );
+        Serial.print( ' ' );
+    }
+#endif
+
+    Serial.print( sensorValue );
+    Serial.print( ' ' );
+    Serial.print( running_mean_ );
+    Serial.print( '\n' );
+    if( sensorValue > (1.1 * running_mean_) )
     {
         Serial.println( "Trigger" );
-        digitalWrite( 13, HIGH );
-        delay( 100 );
+        for (size_t i = 0; i < 5; i++) 
+        {
+            digitalWrite( 13, HIGH );
+            delay( 30 );
+            digitalWrite( 13, LOW);
+            delay( 30 );
+            
+        }
     }
     digitalWrite( 13, LOW );
-    delay(1);        // delay in between reads for stability
+    delay(1); 
 }
